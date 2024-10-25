e15922a0945218b73a17db4ad8ca6579592be92e
Nadya Sava Maritza <nadya.sava@student.umn.ac.id>
2024-10-25 11:38:53 +0700

    gradle implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ android {
 
 dependencies {
 
+    implementation("nl.bryanderidder:themed-toggle-button-group:1.4.1")
+
     implementation ("com.google.android.material:material:1.12.0")
     implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation ("androidx.cardview:cardview:1.0.0")
